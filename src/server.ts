4f058d90a01b6d1157f66e8857f5fab10f285682
Arthemus <arthemus.moreira@gmail.com>
2019-06-25 12:11:35 +1000

    Define dynamic port.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
+import { PORT } from './config';
+
 import App from './App';
+
 import CategoriesController from './categories/category.controller';
- 
-const app = new App(
-  [
-    new CategoriesController(),
-  ],
-  3003,
-  '0.0.0.0'
-);
- 
+
+const app = new App([new CategoriesController()], PORT, '0.0.0.0');
+
 app.listen();
