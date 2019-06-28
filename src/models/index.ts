57b1549b77dfa6bbab3d21c9a88b3c5ff75858c4
Lucas <csalucasnascimento@gmail.com>
2019-06-28 15:19:15 +1000

    prettier

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,7 @@
-import { Listing } from './listing.model';
-import { ListingData } from './listingData.model';
-import { ListSettings } from './listSettings.model';
-import { ListSettingsParent } from './listSettingsParent.model';
+import { Listing } from "./listing.model";
+import { ListingData } from "./listingData.model";
+import { ListSettings } from "./listSettings.model";
+import { ListSettingsParent } from "./listSettingsParent.model";
 
 export { Listing, ListingData, ListSettings, ListSettingsParent };
 
