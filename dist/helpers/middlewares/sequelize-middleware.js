94a2e5f3905b7527a0dd28fa98e268676b850eef
Arthemus <arthemus.moreira@gmail.com>
2019-07-17 09:52:29 +1000

    Remove dist folder.

--- dist/helpers/middlewares/sequelize-middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (request, response, next) => {
-    console.debug('Running Database middleware.');
-    // todo: Looking for an approach to turn Sync database dynamicaly, to 'NOT' sync in production. [Arthemus]
-    // sequelize.sync();
-    next();
-};
