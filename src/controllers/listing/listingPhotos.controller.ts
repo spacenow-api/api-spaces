7a8d5f1b105874444dda8b16d16724da0c779c74
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-07-27 18:22:37 +1000

    listing photos

---
+++ src/controllers/listing/listingPhotos.controller.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+import authMiddleware from '../../helpers/middlewares/auth-middleware';
+import sequelizeErrorMiddleware from '../../helpers/middlewares/sequelize-error-middleware';
+
+import { ListingPhotos } from '../../models';
+
+class ListingPhotosController {
+	private router = Router();
+
+	constructor() {
+		this.intializeRoutes();
+	}
+
+	private intializeRoutes() {
+		/**
+		 * Get listing Photos by listing ID.
+		 */
+		this.router.get(
+			`/listings/photos/:listingId`,
+			authMiddleware,
+			async (req: Request, res: Response, next: NextFunction) => {
+				try {
+					const photosArray: Array<ListingPhotos> = await ListingPhotos.findAll(
+						{
+							where: { listingId: req.params.listingId },
+							raw: true,
+						},
+					);
+					res.send(photosArray);
+				} catch (err) {
+					sequelizeErrorMiddleware(err, req, res, next);
+				}
+			},
+		);
+	}
+}
+
+export default ListingPhotosController;
