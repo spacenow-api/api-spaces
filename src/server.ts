f2fe5a4513007686f9e11b7c6988bae8d92dcaea
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-07-27 19:38:56 +1000

    listing pgotos controller

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,27 @@
-import { PORT } from "./config";
+import { PORT } from './config';
 
-import App from "./App";
+import App from './App';
 
-import HealthController from "./controllers/health/health.controller";
-import ListingController from "./controllers/listing/listing.controller";
-import ListingSettingsController from "./controllers/listing/listingSettings.controller";
-import ListingAmenities from "./controllers/listing/listingAmenities.controller";
-import ListingRulesController from "./controllers/listing/listingRules.controller";
-import ListingAccessDaysController from "./controllers/listing/listingAccessDays.controller";
+import HealthController from './controllers/health/health.controller';
+import ListingController from './controllers/listing/listing.controller';
+import ListingSettingsController from './controllers/listing/listingSettings.controller';
+import ListingAmenities from './controllers/listing/listingAmenities.controller';
+import ListingRulesController from './controllers/listing/listingRules.controller';
+import ListingAccessDaysController from './controllers/listing/listingAccessDays.controller';
+import ListingPhotosController from './controllers/listing/listingPhotos.controller';
 
 const app = new App(
-  [
-    new HealthController(),
-    new ListingController(),
-    new ListingSettingsController(),
-    new ListingAmenities(),
-    new ListingRulesController(),
-    new ListingAccessDaysController()
-  ],
-  PORT,
-  "0.0.0.0"
+	[
+		new HealthController(),
+		new ListingController(),
+		new ListingSettingsController(),
+		new ListingAmenities(),
+		new ListingRulesController(),
+		new ListingAccessDaysController(),
+		new ListingPhotosController(),
+	],
+	PORT,
+	'0.0.0.0',
 );
 
 app.listen();
