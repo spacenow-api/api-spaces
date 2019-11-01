c26284dc7b4f1929fbe7029cb43b173517530fec
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-11-01 14:38:19 +1100

    get all hosts

--- src/controllers/listingLegacy/listing.controller.ts
+++ src/controllers/listingLegacy/listing.controller.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { subDays, format } from "date-fns";
 import * as config from "../../config";
 import authMiddleware from "../../helpers/middlewares/auth-middleware";
 import HttpException from "../../helpers/exceptions/HttpException";
 import sequelizeErrorMiddleware from "../../helpers/middlewares/sequelize-error-middleware";
 
-import { Listing, Location } from "../../models";
+import { Listing, Location, UserProfile } from "../../models";
 
 class ListingLegacyController {
   private router = Router();
@@ -15,6 +16,8 @@ class ListingLegacyController {
 
   private intializeRoutes() {
     this.router.get(`/listings`, authMiddleware, this.getAllListings);
+    this.router.get(`/listings/count/hosts`, authMiddleware, this.getAllHosts);
+    this.router.get(`/listings/count/date`, authMiddleware, this.getAllListingsByDate);
   }
 
   getAllListings = async (
@@ -44,6 +47,56 @@ class ListingLegacyController {
 
     return response.send(data);
   };
+
+  getAllHosts = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const data = await Listing.count({
+        distinct: true,
+        col: 'userId'
+      });
+      response.send({ count: data });
+    } catch (error) {
+      sequelizeErrorMiddleware(error, request, response, next);
+    }
+  };
+
+  getAllListingsByDate = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    const days = request.query.days
+    const sudDays = subDays(new Date(), days)
+    console.log(subDays);
+    const where = { 
+      where: { 
+        createdAt: { $gte: `${sudDays}` }
+      } 
+    }
+    try {
+      const data = await Listing.count(where);
+      response.send(data);
+    } catch (error) {
+      sequelizeErrorMiddleware(error, request, response, next);
+    }
+  };
+
+  // getAllListingsByCategory = async (
+  //   request: Request,
+  //   response: Response,
+  //   next: NextFunction
+  // ) => {
+
+  //   const where = { where: { listSettingsParentId } }
+  //   const data = await Listing.count(where);
+
+  //   return response.send(data);
+  // };
+
 }
 
 export default ListingLegacyController;
