3ca3280e853d89ef00255ad2f89df1003ee13dce
Arthemus <arthemus.moreira@gmail.com>
2019-11-11 12:36:19 +1100

    Filtering to only get images.

--- src/controllers/listing/listingPhotos.controller.ts
+++ src/controllers/listing/listingPhotos.controller.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express';
 
-import authMiddleware from '../../helpers/middlewares/auth-middleware';
 import sequelizeErrorMiddleware from '../../helpers/middlewares/sequelize-error-middleware';
 
 import { ListingPhotos } from '../../models';
 
 class ListingPhotosController {
+
 	private router = Router();
 
 	constructor() {
@@ -16,22 +16,16 @@ class ListingPhotosController {
 		/**
 		 * Get listing Photos by listing ID.
 		 */
-		this.router.get(
-			`/listings/photos/:listingId`,
-			async (req: Request, res: Response, next: NextFunction) => {
-				try {
-					const photosArray: Array<ListingPhotos> = await ListingPhotos.findAll(
-						{
-							where: { listingId: req.params.listingId },
-							raw: true,
-						},
-					);
-					res.send(photosArray);
-				} catch (err) {
-					sequelizeErrorMiddleware(err, req, res, next);
-				}
-			},
-		);
+		this.router.get('/listings/photos/:listingId', async (req: Request, res: Response, next: NextFunction) => {
+			try {
+				const photosArray: Array<ListingPhotos> = await ListingPhotos.findAll({
+					where: { listingId: req.params.listingId, type: 'image/jpeg' }
+				});
+				res.send(photosArray);
+			} catch (err) {
+				sequelizeErrorMiddleware(err, req, res, next);
+			}
+		});
 	}
 }
 
