159cadebe25e776c3739cc54f89bfb33fee9c002
Arthemus <arthemus.moreira@gmail.com>
2019-12-17 14:00:56 +1100

    Caching categories result.

--- src/controllers/categories/category.controller.ts
+++ src/controllers/categories/category.controller.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, NextFunction } from "express";
+import NodeCache from 'node-cache';
 
 import sequelizeErrorMiddleware from "../../helpers/middlewares/sequelize-error-middleware";
+
 import {
   ListSettings,
   ListSettingsParent,
@@ -9,7 +11,9 @@ import {
 
 const REFERENCE_CATEGORIES_ID: number = 111;
 
-export const _getCategories = () => {
+const CACHE_KEY = '_categories_full_'
+
+export const _getCategories = (): Promise<Array<ListSettings>> => {
   const include = {
     include: [
       {
@@ -26,22 +30,24 @@ export const _getCategories = () => {
       }
     ]
   };
-
   const where = {
     where: {
       typeId: REFERENCE_CATEGORIES_ID
     }
   };
-
   try {
-    return ListSettings.findAll({ ...where, ...include });
+    return ListSettings.findAll({ ...where, ...include, raw: true });
   } catch (err) {
     return err;
   }
 };
 
 class CategoriesController {
+
   private router: Router = Router();
+
+  // Standard expiration time for 3 days...
+  private cache: NodeCache = new NodeCache({ stdTTL: 259200 });
 
   constructor() {
     this.intializeRoutes();
@@ -52,9 +58,20 @@ class CategoriesController {
   }
 
   getCategories = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await _getCategories();
-    if (data.err) sequelizeErrorMiddleware(data.err, req, res, next);
-    res.send(data);
+    try {
+      let data: any = this.cache.get(CACHE_KEY);
+      if (!data) {
+        data = await _getCategories();
+        if (data.err) {
+          sequelizeErrorMiddleware(data.err, req, res, next);
+          return;
+        }
+        this.cache.set(CACHE_KEY, data);
+      }
+      res.send(data);
+    } catch (err) {
+      sequelizeErrorMiddleware(err, req, res, next);
+    }
   };
 }
 
