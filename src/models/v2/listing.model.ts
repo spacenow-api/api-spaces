584e309483d39fd1518ae6cf04920de23080a764
Lucas <csalucasnascimento@gmail.com>
2020-01-24 16:17:11 +1100

    API version 2 for new listing process

---
+++ src/models/v2/listing.model.ts
@@ -0,0 +1,134 @@
+import {
+  Table,
+  Column,
+  AutoIncrement,
+  Model,
+  CreatedAt,
+  UpdatedAt,
+  PrimaryKey,
+  AllowNull,
+  Default,
+  DataType,
+  BelongsTo,
+  ForeignKey,
+  HasOne,
+  HasMany,
+  AfterCreate,
+  AfterUpdate
+} from "sequelize-typescript";
+
+import { UserProfile } from "../";
+import {
+  V2ListingPhotos,
+  V2ListingAccessDays,
+  V2ListingData,
+  V2ListingSteps,
+  V2ListingAmenities,
+  V2ListingRules,
+  V2ListingExceptionDates
+} from "./";
+
+import axios from "axios";
+import { emailsApi } from "../../config";
+
+@Table({
+  tableName: "Listing"
+})
+export class V2Listing extends Model<V2Listing> {
+  @PrimaryKey
+  @AutoIncrement
+  @AllowNull(false)
+  @Column
+  id!: number;
+
+  @ForeignKey(() => UserProfile)
+  @AllowNull(false)
+  @Column
+  userId!: string;
+
+  @Column
+  locationId?: number;
+
+  @Column
+  listSettingsParentId?: number;
+
+  @Column
+  bookingPeriod?: string;
+
+  @Column
+  title?: string;
+
+  @AllowNull(false)
+  @Default("instant")
+  @Column(DataType.ENUM("instant", "request", "poa"))
+  bookingType?: string;
+
+  @AllowNull(false)
+  @Default(false)
+  @Column
+  isPublished?: boolean;
+
+  @AllowNull(false)
+  @Default(false)
+  @Column
+  isReady?: boolean;
+
+  @Default("active")
+  @Column(DataType.ENUM("active", "deleted", "claimed"))
+  status?: string;
+
+  @AllowNull(false)
+  @CreatedAt
+  @Column
+  createdAt!: Date;
+
+  @AllowNull(false)
+  @UpdatedAt
+  @Column
+  updatedAt!: Date;
+
+  @HasOne(() => V2ListingData, "listingId")
+  listingData!: V2ListingData;
+
+  @HasOne(() => V2ListingAccessDays, "listingId")
+  accessDays!: V2ListingAccessDays;
+
+  @BelongsTo(() => UserProfile)
+  host!: UserProfile;
+
+  @HasMany(() => V2ListingPhotos, "listingId")
+  photos!: V2ListingPhotos[];
+
+  @HasMany(() => V2ListingAmenities, "listingId")
+  amenities!: V2ListingAmenities[];
+
+  @HasMany(() => V2ListingRules, "listingId")
+  rules!: V2ListingRules[];
+
+  @HasMany(() => V2ListingExceptionDates, "listingId")
+  exceptionDates!: V2ListingExceptionDates[];
+
+  @AfterCreate
+  static createListingSteps(instance: V2Listing) {
+    V2ListingSteps.create({ listingId: instance.id });
+  }
+
+  @AfterCreate
+  static createListingData(instance: V2Listing) {
+    V2ListingData.create({ listingId: instance.id });
+  }
+
+  @AfterCreate
+  static createListingAvailability(instance: V2Listing) {
+    V2ListingAccessDays.create({ listingId: instance.id });
+  }
+
+  @AfterUpdate
+  static sendPublishEmail(instance: V2Listing) {
+    if (
+      instance.previous("isPublished") === false &&
+      instance.isPublished === true
+    )
+      axios.post(`${emailsApi}/email/listing/${instance.id}/publish`);
+  }
+}
